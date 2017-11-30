4d06b8a61519106475e204b4a1e96c72d728432a
Glatoo <darkisharki@gmail.com>
2017-11-30 20:18:39 +0100

    PrvyDen

--- Main/main.py
+++ Main/main.py
@@ -3,11 +3,16 @@ import pygame
 WIDTH = 800
 HEIGHT = 600
 WHITE = (255, 255, 255)
+RED = (255,0,0)
 PLAY = True
 pygame.init()
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Janko a Marienka')
 clock = pygame.time.Clock()
+
+
+def car(x, y):
+    pygame.draw.rect(window, RED, (x, y, x + 50, y + 50), 2)
 
 
 while PLAY:
@@ -17,6 +22,10 @@ while PLAY:
             PLAY = False
 
     window.fill(WHITE)
+    car(200, 200)
     pygame.display.update()
+    clock.tick(60)
+
+
 
 
