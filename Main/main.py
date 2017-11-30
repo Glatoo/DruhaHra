c66c82645679555b698e119b01a18ceb5226e85e
Glatoo <darkisharki@gmail.com>
2017-11-30 20:07:45 +0100

    Druhe Kroky

--- Main/main.py
+++ Main/main.py
@@ -2,5 +2,21 @@ import pygame
 
 WIDTH = 800
 HEIGHT = 600
+WHITE = (255, 255, 255)
+PLAY = True
 pygame.init()
 window = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Janko a Marienka')
+clock = pygame.time.Clock()
+
+
+while PLAY:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            PLAY = False
+
+    window.fill(WHITE)
+    pygame.display.update()
+
+
